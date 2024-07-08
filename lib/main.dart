82313c6d71ef53a94aeeea2dcc5809b7bd3fbe25
ShantanuKH <khadseshantanu02@gmail.com>
2024-07-08 10:38:26 +0530

    Created User Channel UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youtube_clone/cores/screens/loader.dart';
+import 'package:youtube_clone/features/auth/channel/my_channel/pages/user_channel/pages/user_channel_page.dart';
 import 'package:youtube_clone/features/auth/pages/channel_settings.dart';
 import 'package:youtube_clone/features/auth/pages/login_page.dart';
 import 'package:youtube_clone/features/auth/channel/my_channel/pages/my_channel_screen.dart';
@@ -57,7 +58,7 @@ class MyApp extends ConsumerWidget {
                 } else if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Loader();
                 }
-                return  MyChannelSettings();
+                return  UserChannelPage();
               },
             );
           }),
