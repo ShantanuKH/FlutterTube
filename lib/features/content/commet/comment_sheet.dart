68193d5cb98fbce057c051cdbc6f7add0a3bcffc
ShantanuKH <khadseshantanu02@gmail.com>
2024-07-09 15:30:23 +0530

    Display real time comments in comment section

--- lib/features/content/commet/comment_sheet.dart
+++ lib/features/content/commet/comment_sheet.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/screens/error_page.dart';
+import 'package:youtube_clone/cores/screens/loader.dart';
 import 'package:youtube_clone/features/auth/provider/user_provider.dart';
+import 'package:youtube_clone/features/content/commet/comment_tile.dart';
+import 'package:youtube_clone/features/upload/comments/comment_model.dart';
 
 import 'package:youtube_clone/features/upload/comments/comment_repository.dart';
 import 'package:youtube_clone/features/upload/long_video/videoModel.dart';
@@ -49,12 +55,43 @@ class _CommentSheetState extends ConsumerState<CommentSheet> {
             child: const Text(
                 "Remember to keep comments respectful and to follow our community guidelines"),
           ),
-          const SizedBox(height: 10),
+
+          // To show comments in the screen...also we want to specify comments on the specific video so we used where condition
+          StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("comments")
+                  .where("videoId", isEqualTo: widget.video.videoId)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData || snapshot.data == null) {
+                  return ErrorPage();
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return Loader();
+                }
+
+                final commentsMap = snapshot.data!.docs;
+                final List<CommentModel> comments = commentsMap
+                    .map((comment) => CommentModel.fromMap(comment.data()))
+                    .toList();
+                return Expanded(
+                  child: ListView.builder(
+                      itemCount: comments.length,
+                      itemBuilder: (context, index) {
+                        return CommentTile(comment: comments[index]);
+                      }),
+                );
+              }),
+
+          
           Row(
             children: [
-              const CircleAvatar(
+               CircleAvatar(
                 radius: 18,
                 backgroundColor: Colors.grey,
+               backgroundImage: CachedNetworkImageProvider(
+                    user.value!.profilePic
+                  ),
               ),
               const SizedBox(width: 10),
               Expanded(
@@ -69,12 +106,14 @@ class _CommentSheetState extends ConsumerState<CommentSheet> {
                       contentPadding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 5),
                       suffixIcon: InkWell(
-                          onTap: ()async {
-                           await ref.watch(commentProvider).uploadCommentToFireStore(
-                                commentText: commentController.text,
-                                videoId: widget.video.videoId,
-                                profilePic: user.value!.profilePic,
-                                displayName: user.value!.displayName);
+                          onTap: () async {
+                            await ref
+                                .watch(commentProvider)
+                                .uploadCommentToFireStore(
+                                    commentText: commentController.text,
+                                    videoId: widget.video.videoId,
+                                    profilePic: user.value!.profilePic,
+                                    displayName: user.value!.displayName);
                           },
                           child: Icon(Icons.send))),
                 ),
--- lib/features/content/commet/comment_tile.dart
+++ lib/features/content/commet/comment_tile.dart
@@ -1,36 +1,62 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+import 'package:youtube_clone/features/upload/comments/comment_model.dart';
+
 class CommentTile extends StatelessWidget {
-  const CommentTile({super.key});
+  final CommentModel comment;
+  const CommentTile({
+    Key? key,
+    required this.comment,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
               CircleAvatar(
                 radius: 15,
                 backgroundColor: Colors.grey,
-          
+                backgroundImage: CachedNetworkImageProvider(comment.profilePic),
               ),
-              Text("Shantanu", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      comment.displayName,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    const Text(
+                      "a moment ago",
+                      style: TextStyle(color: Colors.blueGrey, fontSize: 12),
+                    ),
+                  ],
+                ),
               ),
-              ),
-              Text("a moment ago"),
-              Spacer(),
-              Icon(Icons.more_vert)
+              const Icon(Icons.more_vert),
             ],
           ),
-
-          Text("First Comment"),
+          const SizedBox(height: 8),
+          Padding(
+            padding: 
+            EdgeInsets.only(
+              left:MediaQuery.sizeOf(context).width*0.1, 
+            ),
+            child: Text(comment.commentText),
+            
+          ),
+          Divider(color: Colors.grey.shade300,)
         ],
-
       ),
-
     );
   }
 }
